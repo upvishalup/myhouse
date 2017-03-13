12bcded96bbebbd73aa009bd6574379b052e8af2
Sunil <Vishal>
2017-03-13 23:47:54 +0530

    Map markers

--- assets/js/extra.js
+++ assets/js/extra.js
@@ -1,6 +1,8 @@
 
 
 window.onload = function(){
+   
+
   document.getElementById("agent-email-1").onmouseover = function(){
       document.getElementById("agent-email-1").style.display = "none";
       document.getElementById("agent-email-2").style.display = "block";
@@ -26,189 +28,18 @@ window.onload = function(){
 var app = angular.module("Location", []);
 
 app.controller("LocationController", function($scope){
-      
-      $scope.data = {
-      "Gastronomy" : [
-            {
-            "title" : "Aromi",
-            "name" : "Náměstí Míru 6, Prague 2",
-            "location" : [50.076149, 14.436372],
-            "description" : "",
-            "thubnail" : "",
-            "image" : "assets/img/content/g1.jpg",
-            "isOpened" : false
-            },
-            {
-            "title" : "Bruxx",
-            "name" : "Náměstí Míru 9, Prague 2",
-            "location" : [50.075441, 14.438027],
-            "description" : "",
-            "thubnail" : "",
-            "image" : "assets/img/content/g1.jpg",
-            "isOpened" : false
-            },
-            {
-            "title" : "Yamato",
-            "name" : "U Kanálky 14, Prague 2",
-            "location" : [50.078406, 14.446084],
-            "description" : "",
-            "thubnail" : "",
-            "image" : "assets/img/content/g1.jpg",
-            "isOpened" : false
-            },
-            {
-            "title" : "Vinohrady Brewery",
-            "name" : "Korunní 106, Prague 2",
-            "location" : [50.075188, 14.457489],
-            "description" : "",
-            "thubnail" : "",
-            "image" : "assets/img/content/g1.jpg",
-            "isOpened" : false
-            }
-      ],
-      "Cafe & Bars" : [
-            {
-            "title" : "Bar and Books",
-            "name" : "Mánesova 64, Prague 2",
-            "location" : [50.077595, 14.444172],
-            "description" : "",
-            "thubnail" : "",
-            "image" : "assets/img/content/g1.jpg",
-            "isOpened" : false
-            },
-            {
-            "title" : "Zizkov Tower Bar",
-            "name" : "Mahlerovy sady 1, Prague 3",
-            "location" : [50.081047, 14.451099],
-            "description" : "",
-            "thubnail" : "",
-            "image" : "assets/img/content/g1.jpg",
-            "isOpened" : false
-            },
-            {
-            "title" : "La Bottega Gastronomica",
-            "name" : "Ondříčkova 17, Prague 3",
-            "location" : [50.079816, 14.450390],
-            "description" : "",
-            "thubnail" : "",
-            "image" : "assets/img/content/g1.jpg",
-            "isOpened" : false
-            },{
-            "title" : "La Boheme Cafe",
-            "name" : "Sázavská 32, Prague 2",
-            "location" : [50.076713, 14.440858],
-            "description" : "",
-            "thubnail" : "",
-            "image" : "assets/img/content/g1.jpg",
-            "isOpened" : false
-            },
-            {
-            "title" : "Monolok Cafe",
-            "name" : "Moravská 18, Prague 2",
-            "location" : [50.074226, 14.442974],
-            "description" : "",
-            "thubnail" : "",
-            "image" : "assets/img/content/g1.jpg",
-            "isOpened" : false
-            },
-            {
-            "title" : "Prosekarna",
-            "name" : "Slezská 48, Prague 2",
-            "location" : [50.076143, 14.445368],
-            "description" : "",
-            "thubnail" : "",
-            "image" : "assets/img/content/g1.jpg",
-            "isOpened" : false
-            }
-      ],
-      "Culture" : [
-            {
-            "title" : "The State Opera",
-            "name" : "Wilsonova 4, Prague 1",
-            "location" : [50.080435, 14.432975],
-            "description" : "",
-            "thubnail" : "",
-            "image" : "assets/img/content/g1.jpg",
-            "isOpened" : false
-            },
-            {
-            "title" : "Vinohrady Theatre",
-            "name" : "Náměstí Míru 7, Prague 2",
-            "location" : [50.076380, 14.437040],
-            "description" : "",
-            "thubnail" : "",
-            "image" : "assets/img/content/g1.jpg",
-            "isOpened" : false
-            },
-            {
-            "title" : "Farmers markets",
-            "name" : "Tylovo náměstí, Prague 2",
-            "location" : [50.074982, 14.432051],
-            "description" : "",
-            "thubnail" : "",
-            "image" : "assets/img/content/g1.jpg",
-            "isOpened" : false
-            },{
-            "title" : "Farmers markets and Cultural Events",
-            "name" : "Náměstí Jiřího z Poděbrad, Prague 3",
-            "location" : [50.078298, 14.449394],
-            "description" : "",
-            "thubnail" : "",
-            "image" : "assets/img/content/g1.jpg",
-            "isOpened" : false
-            }
-      ],
-      "University&Shopping" : [
-            {
-            "title" : "The Pavilon",
-            "name" : "Vinohradská 50, Prague 2",
-            "location" : [50.076706, 14.442326],
-            "description" : "",
-            "thubnail" : "",
-            "image" : "assets/img/content/g1.jpg",
-            "isOpened" : false
-            },
-            {
-            "title" : "Atrium Flora",
-            "name" : "Vinohradská 151, Prague 3",
-            "location" : [50.078789, 14.461137],
-            "description" : "",
-            "thubnail" : "",
-            "image" : "assets/img/content/g1.jpg",
-            "isOpened" : false
-            },
-            {
-            "title" : "Prague University of Economy",
-            "name" : "náměstí W. Churchilla 4, Prague 3",
-            "location" : [50.084199, 14.441162],
-            "description" : "",
-            "thubnail" : "",
-            "image" : "assets/img/content/g1.jpg",
-            "isOpened" : false
-            },
-            {
-            "title" : "University of New York Prague",
-            "name" : "Londýnská 41, Prague 2",
-            "location" : [50.073458, 14.433983],
-            "description" : "",
-            "thubnail" : "",
-            "image" : "assets/img/content/g1.jpg",
-            "isOpened" : false
-            }
-         ]
-      };
       $scope.openedAccount = {};
+      $scope.data = initialisedPoi();
+      $scope.keys = Object.keys($scope.data);
       $scope.pois = [];
       $scope.total = 0;
       $scope.opened = 0;
       $scope.keys = Object.keys($scope.data);
       $scope.pois = $scope.data[$scope.keys[0]];
-
       if($scope.pois.length % 2 !== 0){
             $scope.pois[$scope.pois.length - 1].isOpened = true;
             $scope.openedAccount[$scope.pois.length-1] = true;
       }
-
 
       $scope.showPoi = function(key){
             $scope.total = 0;
@@ -220,6 +51,7 @@ app.controller("LocationController", function($scope){
                   $scope.openedAccount[$scope.pois.length-1] = true;
             }
             $("#imageresult").scrollTop = ($("#imageresult").scrollTop + 40);
+            displayLocInMap(getLocations());  
       };
 
       
@@ -271,7 +103,25 @@ app.controller("LocationController", function($scope){
             $scope.checkImageDisplay(currentIndex);      
             
             
+      };
+
+      function getLocations() {
+          var locations = [];
+            for(var i = 0; i < $scope.pois.length; i++){
+                  var locals = {
+                  locId : i+1,
+                  lat: $scope.pois[i].location[0],
+                  lng: $scope.pois[i].location[1]
+                  } 
+                 locations.push(locals);
+           }
+            //console.log("Printing all markers:",locations);
+            return locations;
       }
+
+      window.addEventListener('marker-cliked', function (e) {
+          console.log('printer state changed', e.detail);
+      });
 });
 
 
--- assets/js/map.js
+++ assets/js/map.js
@@ -7,7 +7,7 @@ window.google = window.google || {};
 google.maps = google.maps || {};
 
 (function() {
-
+  initialisedPoi();
   function getScript(src) {
     document.write('<' + 'script src="' + src + '"' +
       ' type="text/javascript"><' + '/script>');
@@ -39,6 +39,7 @@ google.maps = google.maps || {};
 <!-- ================================================== -->
 
 var map = null;
+var locMarker = [];
 jQuery(document).ready(function(){
 
   
@@ -56,29 +57,29 @@ jQuery(document).ready(function(){
           zoom: 13,
           center: myLatLng,
           disableDefaultUI: true,
-          scrollwheel: false,
-          navigationControl: false,
-          mapTypeControl: false,
-          scaleControl: false,
-          draggable: false,
+          scrollwheel: true,
+          navigationControl: true,
+          mapTypeControl: true,
+          scaleControl: true,
+          draggable: true,
           mapTypeControlOptions: {
             mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'usroadatlas']
           }
         };
 
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-    initialiseMarker();
-    displayLocInMap();
+    //initialiseMarker();
+    displayLocInMap(getInitLocations());
     
-   // var img = jQuery('#map-canvas').data('img');
-   /* 
+    var img = jQuery('#map-canvas').data('img');
+    
     var marker = new google.maps.Marker({
         position: myLatLng,
         map: map,
         icon: img,
       title: ''
     });
-    
+    /*
     var contentString = '<div style="max-width: 300px" id="content">'+
         '<div id="bodyContent">'+
       '<h5 class="color-primary"><strong>StylishThemes</strong></h5>' +
@@ -110,8 +111,10 @@ jQuery(document).ready(function(){
 });
 
 // Pop-up Location marker in Google map  
-var markers = [];
+/*
 function initialiseMarker(){
+ // alert("hi");
+  var markers = [];
   var locDetail = {};
   $( ".loc-img" ).each(function( index ) {
     locDetail = {"lat": $( this ).attr("data-lat"),
@@ -119,30 +122,237 @@ function initialiseMarker(){
         "locId": $( this ).attr("data-locid")};
     markers.push(locDetail);          
 });
+  return markers;
+}
+*/
+var displayLocInMap = function (markers) {
+  //alert("hello");
+  //setMapOnAll(null);
+  if(locMarker !='undefined' && locMarker != null)
+  {
+    clearMarkers(locMarker);
+  }
+  
+  var infoWindow = new google.maps.InfoWindow();
+  var lat_lng = new Array();
+  var latlngbounds = new google.maps.LatLngBounds();
+  for (i = 0; i < markers.length; i++) {
+    var data = markers[i];
+    var myLatlng = new google.maps.LatLng(data.lat, data.lng);
+    lat_lng.push(myLatlng);
+    var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      id: data.locId
+    });
+    locMarker.push(marker);
+    google.maps.event.addListener(marker, 'click', function() {    
+        var divId =   this.id;     
+        var event = new CustomEvent('marker-cliked', { 'detail': divId }); 
+        window.dispatchEvent(event);  
+    }); 
+  }
+  console.log( markers);
+
 }
 
-var displayLocInMap = function () {
-var infoWindow = new google.maps.InfoWindow();
-var lat_lng = new Array();
-var latlngbounds = new google.maps.LatLngBounds();
-for (i = 0; i < markers.length; i++) {
-  var data = markers[i]
-  var myLatlng = new google.maps.LatLng(data.lat, data.lng);
-  lat_lng.push(myLatlng);
-  var marker = new google.maps.Marker({
-  position: myLatlng,
-  map: map,
-  id: data.locId
-  });
-  google.maps.event.addListener(marker, 'click', function() {    
-        var divId =   this.id;     
-        var divObj = $("div.loc-img[data-locid = '"+divId+"']"); 
-        divObj.css("border", "3px solid black");
-
-        }); 
+function clearMarkers(markers) {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(null);
   }
 }
 
 <!-- ================================================== -->
 <!-- =============== END GOOGLE MAP SETTINGS ================ -->
 <!-- ================================================== -->
+
+
+function initialisedPoi(){
+  var poiImageJsonData = {
+      "Gastronomy" : [
+            {
+            "title" : "Aromi",
+            "name" : "Náměstí Míru 6, Prague 2",
+            "location" : [50.076149, 14.436372],
+            "description" : "",
+            "thubnail" : "",
+            "image" : "assets/img/content/g1.jpg",
+            "isOpened" : false
+            },
+            {
+            "title" : "Bruxx",
+            "name" : "Náměstí Míru 9, Prague 2",
+            "location" : [50.075441, 14.438027],
+            "description" : "",
+            "thubnail" : "",
+            "image" : "assets/img/content/g1.jpg",
+            "isOpened" : false
+            },
+            {
+            "title" : "Yamato",
+            "name" : "U Kanálky 14, Prague 2",
+            "location" : [50.078406, 14.446084],
+            "description" : "",
+            "thubnail" : "",
+            "image" : "assets/img/content/g1.jpg",
+            "isOpened" : false
+            },
+            {
+            "title" : "Vinohrady Brewery",
+            "name" : "Korunní 106, Prague 2",
+            "location" : [50.075188, 14.457489],
+            "description" : "",
+            "thubnail" : "",
+            "image" : "assets/img/content/g1.jpg",
+            "isOpened" : false
+            }
+      ],
+      "Cafe & Bars" : [
+            {
+            "title" : "Bar and Books",
+            "name" : "Mánesova 64, Prague 2",
+            "location" : [50.077595, 14.444172],
+            "description" : "",
+            "thubnail" : "",
+            "image" : "assets/img/content/g1.jpg",
+            "isOpened" : false
+            },
+            {
+            "title" : "Zizkov Tower Bar",
+            "name" : "Mahlerovy sady 1, Prague 3",
+            "location" : [50.081047, 14.451099],
+            "description" : "",
+            "thubnail" : "",
+            "image" : "assets/img/content/g1.jpg",
+            "isOpened" : false
+            },
+            {
+            "title" : "La Bottega Gastronomica",
+            "name" : "Ondříčkova 17, Prague 3",
+            "location" : [50.079816, 14.450390],
+            "description" : "",
+            "thubnail" : "",
+            "image" : "assets/img/content/g1.jpg",
+            "isOpened" : false
+            },{
+            "title" : "La Boheme Cafe",
+            "name" : "Sázavská 32, Prague 2",
+            "location" : [50.076713, 14.440858],
+            "description" : "",
+            "thubnail" : "",
+            "image" : "assets/img/content/g1.jpg",
+            "isOpened" : false
+            },
+            {
+            "title" : "Monolok Cafe",
+            "name" : "Moravská 18, Prague 2",
+            "location" : [50.074226, 14.442974],
+            "description" : "",
+            "thubnail" : "",
+            "image" : "assets/img/content/g1.jpg",
+            "isOpened" : false
+            },
+            {
+            "title" : "Prosekarna",
+            "name" : "Slezská 48, Prague 2",
+            "location" : [50.076143, 14.445368],
+            "description" : "",
+            "thubnail" : "",
+            "image" : "assets/img/content/g1.jpg",
+            "isOpened" : false
+            }
+      ],
+      "Culture" : [
+            {
+            "title" : "The State Opera",
+            "name" : "Wilsonova 4, Prague 1",
+            "location" : [50.080435, 14.432975],
+            "description" : "",
+            "thubnail" : "",
+            "image" : "assets/img/content/g1.jpg",
+            "isOpened" : false
+            },
+            {
+            "title" : "Vinohrady Theatre",
+            "name" : "Náměstí Míru 7, Prague 2",
+            "location" : [50.076380, 14.437040],
+            "description" : "",
+            "thubnail" : "",
+            "image" : "assets/img/content/g1.jpg",
+            "isOpened" : false
+            },
+            {
+            "title" : "Farmers markets",
+            "name" : "Tylovo náměstí, Prague 2",
+            "location" : [50.074982, 14.432051],
+            "description" : "",
+            "thubnail" : "",
+            "image" : "assets/img/content/g1.jpg",
+            "isOpened" : false
+            },{
+            "title" : "Farmers markets and Cultural Events",
+            "name" : "Náměstí Jiřího z Poděbrad, Prague 3",
+            "location" : [50.078298, 14.449394],
+            "description" : "",
+            "thubnail" : "",
+            "image" : "assets/img/content/g1.jpg",
+            "isOpened" : false
+            }
+      ],
+      "University&Shopping" : [
+            {
+            "title" : "The Pavilon",
+            "name" : "Vinohradská 50, Prague 2",
+            "location" : [50.076706, 14.442326],
+            "description" : "",
+            "thubnail" : "",
+            "image" : "assets/img/content/g1.jpg",
+            "isOpened" : false
+            },
+            {
+            "title" : "Atrium Flora",
+            "name" : "Vinohradská 151, Prague 3",
+            "location" : [50.078789, 14.461137],
+            "description" : "",
+            "thubnail" : "",
+            "image" : "assets/img/content/g1.jpg",
+            "isOpened" : false
+            },
+            {
+            "title" : "Prague University of Economy",
+            "name" : "náměstí W. Churchilla 4, Prague 3",
+            "location" : [50.084199, 14.441162],
+            "description" : "",
+            "thubnail" : "",
+            "image" : "assets/img/content/g1.jpg",
+            "isOpened" : false
+            },
+            {
+            "title" : "University of New York Prague",
+            "name" : "Londýnská 41, Prague 2",
+            "location" : [50.073458, 14.433983],
+            "description" : "",
+            "thubnail" : "",
+            "image" : "assets/img/content/g1.jpg",
+            "isOpened" : false
+            }
+         ]
+      };
+  return poiImageJsonData;
+}
+
+function getInitLocations() {
+    var poisAll = initialisedPoi();
+    var key = Object.keys(poisAll)[0];
+    var pois = poisAll[key];
+    var locations = [];
+      for(var i = 0; i < pois.length; i++){
+            var locals = {
+            locId : i+1,
+            lat: pois[i].location[0],
+            lng: pois[i].location[1]
+            } 
+           locations.push(locals);
+     }
+    return locations;
+}
