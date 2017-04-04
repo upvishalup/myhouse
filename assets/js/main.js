f148bee2174be5c3b464f598fc2de234e394bbf7
Vishal Upadhyay <vishal.upadhyay@teewe.in>
2017-04-05 00:00:52 +0530

    loader removed and

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,6 +17,9 @@ jQuery(document).ready(function(){
 		}
 	});
 
+	if(navigator.userAgent.indexOf('Mac') > 0)
+		$('body').addClass('mac-os');
+
 	jQuery(window).stellar({
   	positionProperty: 'transform',
   	horizontalScrolling: false
