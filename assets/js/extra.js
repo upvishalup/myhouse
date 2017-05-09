27887864d1ba034b20528879201ce57444955c9f
Vishal Upadhyay <vishal.upadhyay@teewe.in>
2017-05-10 01:26:39 +0530

    mobile view added

--- assets/js/extra.js
+++ assets/js/extra.js
@@ -1,7 +1,10 @@
 jQuery(document).ready(function(){
   //$("#mapimageresult").stickit({ top: 0, bottom: 30,zIndex: 1000 });
   //$("#mapresult").stickit({ top: 18, zIndex: 100});
-
+  $('.navbar-collapse a').click(function(){
+      $(".navbar-collapse").collapse('hide');
+  });
+  
   $( window ).resize(function() {
     if($(window).width() < 1030){
       $('#mapresult').stickit('destroy');
