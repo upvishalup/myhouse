4add6e2753a1bd5b770777a85afbfc1a709ff1e1
Vishal Upadhyay <vishal.upadhyay@teewe.in>
2017-05-09 21:23:13 +0530

    revision changes

--- assets/js/extra.js
+++ assets/js/extra.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function(){
-  $("#mapimageresult").stickit({ top: 0, bottom: 30,zIndex: 1000 });
-  $("#mapresult").stickit({ top: 18, zIndex: 100});
+  //$("#mapimageresult").stickit({ top: 0, bottom: 30,zIndex: 1000 });
+  //$("#mapresult").stickit({ top: 18, zIndex: 100});
 
   $( window ).resize(function() {
     if($(window).width() < 1030){
